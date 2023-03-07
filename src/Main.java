0a186d7b6b4cef0ab52c6a34565ab9b15392d2d0
Professional <new99kpicem@gmail.com>
2023-03-08 01:38:08 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
+import javax.lang.model.element.VariableElement;
 import java.util.Scanner;
 public class Main {
-    public static void leapYear(int year) {
+    public static void leapYear(int yearCalc) {
         int leapYear, notLeapYear, leapYearPlus;
-        year = 500;
-        leapYear = year % 4;
-        leapYearPlus = year % 400;
-        notLeapYear = year % 100;
+        leapYear = yearCalc % 4;
+        leapYearPlus = yearCalc % 400;
+        notLeapYear = yearCalc % 100;
         switch (leapYear) {
             case 0:
                 switch (leapYearPlus) {
                     case 0:
-                        System.out.println(year + " год является високосным");
+                        System.out.println(yearCalc + " год является високосным");
                         break;
                     default:
                         if (notLeapYear == 0) {
-                            System.out.println(year + " год не является високосным");
+                            System.out.println(yearCalc + " год не является високосным");
                         } else {
-                            System.out.println(year + " год является високосным");
+                            System.out.println(yearCalc + " год является високосным");
                         }
                 }
                 break;
             default:
-                System.out.println(year + " год не является високосным");
+                System.out.println(yearCalc + " год не является високосным");
         }
     }
     public static void main(String[] args) {
@@ -33,7 +33,7 @@ public class Main {
         System.out.println("Задача 1(Бывшая 3)");
         Scanner y = new Scanner(System.in);
         System.out.println("Введите год ");
-        String year = s.nextLine();
-
+        int year = y.nextInt();
+        leapYear(year);
     }
 }
